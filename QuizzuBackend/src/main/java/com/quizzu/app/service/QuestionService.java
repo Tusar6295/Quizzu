af5972213dac1319e4fff6050fc0e7426232bdb5
Tusar Agarwal <tusara@trainee.nrifintech.com>
2024-05-30 18:46:36 +0530

    refactor

--- QuizzuBackend/src/main/java/com/quizzu/app/service/QuestionService.java
+++ QuizzuBackend/src/main/java/com/quizzu/app/service/QuestionService.java
@@ -12,6 +12,7 @@ import com.quizzu.app.repo.QuizRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -46,5 +47,11 @@ public class QuestionService {
         }
 
         return savedQuestion;
+    }
+
+
+    public List<Question> getAllQuestions(Long id)
+    {
+        return this.questionRepository.findByQuizId(id);
     }
 }
